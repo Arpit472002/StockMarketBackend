c5b702e59e26fb22129ca35b6cee2893419aea98
Arpit Shah <sarpit623@gmail.com>
2024-02-23 19:35:31 +0530

    DirectorLogic

--- game/gamestate.py
+++ game/gamestate.py
@@ -22,9 +22,11 @@ class Gamestate:
         self.netChangeInCompanyByUsers={}
         self.adminId=0
         self.chairman={}
+        self.director={}
 
         for i in Companies:
             self.chairman[i["id"]]=None
+            self.director[i["id"]]=[]
             self.companyValues[i["id"]]={"companyShareValue":i["startingPrice"],
                                       "stocksAvailable":200000}
             self.priceBook[i["id"]]=[i["startingPrice"]]
@@ -38,7 +40,7 @@ class Gamestate:
             self.userState[i] = {
                 "id":i,
                 "username":playersName[i],
-                "cashInHand": 8000000,
+                "cashInHand": 800000,
                 "cashInStocks":0,
                 "holdings": {},
                 "cardsHeld": [],
@@ -114,6 +116,18 @@ class Gamestate:
             if playerIndex!=-1:
                 self.userState[playerIndex]["cardsHeld"].pop(cardIndex)
 
+    def applyDirector(self):
+        for company in Companies:
+            # if self.chairman[company["id"]]!=None:
+            for dir in self.director[company["id"]]:
+                cardIndex = -1
+                val = 0
+                for index,card in enumerate(self.userState[dir]["cardsHeld"]):
+                    if card["type"] == "NORMAL" and card["companyId"]==company["id"] and card["netChange"] < val:
+                        cardIndex = index
+                        val =  card["netChange"]
+                if cardIndex!=-1:
+                    self.userState[dir]["cardsHeld"].pop(cardIndex) 
 
     def calculateNewStockPrice(self):
         # CompanyId -> [netChange] |  netChange by a user in a company is being calculated 
@@ -121,8 +135,8 @@ class Gamestate:
         totalChangeInCompany = [0 for _ in range(7)]
         for i in Companies:
             self.netChangeInCompanyByUsers[i["id"]]=[0 for _ in range(self.noOfPlayers)]
-
-        self.applyChairman()
+        # self.applyDirector()
+        # self.applyChairman()
         for i in range(self.noOfPlayers):
             for card in self.userState[i]["cardsHeld"]:
                 if card["type"] == "NORMAL":
@@ -259,12 +273,26 @@ class Gamestate:
                 self.chairman[transaction["companyId"]] = None
                 for idx in range(self.noOfPlayers):
                     if self.userState[idx]["holdings"][transaction["companyId"]]>=100000:
-                        self.chairman[transaction["companyId"]] = idx
+                        self.chairman[transaction["companyId"]] = idx       
 
         elif transaction["companyId"]>0:
             if self.chairman[transaction["companyId"]]==None and self.userState[transaction["userId"]]["holdings"][transaction["companyId"]]>=100000:
                 self.chairman[transaction["companyId"]] = transaction["userId"]
         
+        #Director Change Logic
+        if transaction["type"]=="SELL":
+            if transaction["userId"] in self.director[transaction["companyId"]] and self.userState[transaction["userId"]]["holdings"][transaction["companyId"]]<50000:
+                self.director[transaction["companyId"]].remove(transaction["userId"])
+                for idx in range(self.noOfPlayers):
+                    if len(self.director[transaction["companyId"]])==2:
+                        break
+                    if self.userState[idx]["holdings"][transaction["companyId"]]>=50000 and self.chairman[transaction["companyId"]]!=idx:
+                        self.director[transaction["companyId"]].append(transaction["userId"])  
+
+        elif transaction["companyId"]>0:
+            if len(self.director[transaction["companyId"]])<2 and self.userState[transaction["userId"]]["holdings"][transaction["companyId"]]>=50000 and self.chairman[transaction["companyId"]]!=transaction["userId"] and transaction["userId"] not in self.director[transaction["companyId"]]:
+                self.director[transaction["companyId"]].append(transaction["userId"])
+
         # Transaction Logic
         self.transactions.insert(0,transaction)
         if len(self.transactions) > 35:
@@ -383,40 +411,43 @@ class Gamestate:
             self.currentTurn,
             self.playerOrder,
             self.circuitValues,
-            self.transactions)
+            self.transactions,
+            self.chairman,
+            self.director)
         )
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, 
             sort_keys=False, indent=4)
     def checkIsAdmin(self,leftPlayerUsername,playersList):
         if len(playersList)==1:
-            return
+            return 0
         for i in self.userState:
             if self.userState[i]["username"]==leftPlayerUsername:
                 if self.adminId==i:
                     self.adminId+=1
                     while self.userState[self.adminId]["username"] not in playersList:
                         self.adminId=(self.adminId+1)%self.noOfPlayers
+                    return 1
 
 
         
-# obj = Gamestate(["bhavik","arun","arpit"],1)
-# obj.startMegaRound()
-# print(obj.playerOrder)
-# obj.passTransaction(obj.playerOrder[obj.currentTurn])
-# obj.buy(obj.playerOrder[obj.currentTurn],1,100000)
-# obj.buy(obj.playerOrder[obj.currentTurn],1,10000)
+obj = Gamestate(["bhavik","arun","arpit"],1)
+obj.startMegaRound()
+print(obj.playerOrder)
+obj.passTransaction(obj.playerOrder[obj.currentTurn])
+obj.buy(obj.playerOrder[obj.currentTurn],1,55000)
+obj.buy(obj.playerOrder[obj.currentTurn],1,10000)
 
-# obj.passTransaction(obj.playerOrder[obj.currentTurn])
-# obj.sell(obj.playerOrder[obj.currentTurn],1,1000)
-# obj.passTransaction(obj.playerOrder[obj.currentTurn])
+obj.passTransaction(obj.playerOrder[obj.currentTurn])
+obj.sell(obj.playerOrder[obj.currentTurn],1,1000)
+obj.passTransaction(obj.playerOrder[obj.currentTurn])
 
-# obj.passTransaction(obj.playerOrder[obj.currentTurn])
-# obj.crystal(obj.playerOrder[obj.currentTurn],"LOAN_ON_STOCK",1,1000)
-# obj.passTransaction(obj.playerOrder[obj.currentTurn])
+obj.passTransaction(obj.playerOrder[obj.currentTurn])
+obj.crystal(obj.playerOrder[obj.currentTurn],"LOAN_ON_STOCK",1,1000)
+obj.passTransaction(obj.playerOrder[obj.currentTurn])
 
-# obj.passTransaction(obj.playerOrder[obj.currentTurn])
-# obj.passTransaction(obj.playerOrder[obj.currentTurn])
-# obj.passTransaction(obj.playerOrder[obj.currentTurn])
+obj.passTransaction(obj.playerOrder[obj.currentTurn])
+obj.passTransaction(obj.playerOrder[obj.currentTurn])
+obj.passTransaction(obj.playerOrder[obj.currentTurn])
 
-# obj.printDetails()
+obj.printDetails()
